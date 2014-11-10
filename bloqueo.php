e10dc5eaefab6aaf16119e44f62fbfbd1dd8b679
jgesu <jcamacho@itecnosolution.com>
2014-11-10 13:45:01 -0600

    Upd

--- bloqueo.php
+++ bloqueo.php
@@ -1,5 +1,5 @@
 <?php 
-$allowed_countries = array("USA", "CA");
+$paises = array("USA", "CA");
 $json = json_decode(file_get_contents("http://ipinfo.io/json"));
 if(in_array($json->country, $allowed_countries)) {
 	echo "Pais Bloqueado!!";
